a3f864bf53dcad1af4c5d734b5abdde6c9789c44
chenkoo <chenkoo@hotmail.com>
2020-07-18 16:53:12 -0400

    ArrayDeque.java modified

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,38 +2,72 @@ public class ArrayDeque<T> {
 
     private T[] items;
     private int size;
+    private int nextFirst;
+    private int nextLast;
 
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
+        nextFirst = 0;
+        nextLast = 1;
     }
 
     public ArrayDeque(T item) {
         items = (T[]) new Object[8];
-        items[0] = item;
+        nextFirst = 0;
+        nextLast = 1;
+        items[nextFirst] = item;
+        nextFirst = items.length - 1;
         size = 1;
     }
 
-    private void resize(int capacity) {
-        T[] itemsNew = (T[]) new Object[capacity];
-        System.arraycopy(itemsNew, 0, items, 0, size);
-        items = itemsNew;
+    private void resize() {
+        if (size == items.length){
+            T[] itemsNew = (T[]) new Object[size * 2];
+            System.arraycopy(itemsNew, 0, items, 0, size);
+            items = itemsNew;
+            nextFirst = items.length - 1;
+            nextLast = size;
+        } else {
+            T[] itemsNew = (T[]) new Object[size/2];
+            if (nextFirst == items.length - 1) {
+                System.arraycopy(itemsNew, 0, items, 0, size);
+                items = itemsNew;
+                nextFirst = items.length - 1;
+                nextLast = size;
+            } else if (nextLast == 0 || nextFirst < nextLast) {
+                System.arraycopy(itemsNew, 0, items, nextFirst + 1, size);
+                items = itemsNew;
+                nextFirst = items.length - 1;
+                nextLast = size;
+            } else {
+                System.arraycopy(itemsNew, 0, items, 0, nextLast);
+                System.arraycopy(itemsNew, nextFirst - size/2 + 1, items, nextFirst + 1,
+                        size - nextLast);
+                items = itemsNew;
+                nextFirst -= size/2;
+            }
+        }
     }
 
     public void addFirst(T item) {
-        if (size == items.length) {
-            resize(size * 2);
-        }
-        items[size] = item;
+        items[nextFirst] = item;
         size += 1;
+        if (size == items.length) {
+            resize();
+        } else {
+            nextFirst -= 1;
+        }
     }
 
     public void addLast(T item) {
-        if (size == items.length) {
-            resize(size * 2);
-        }
-        items[size] = item;
+        items[nextLast] = item;
         size += 1;
+        if (size == items.length) {
+            resize();
+        } else {
+            nextLast += 1;
+        }
     }
 
     public boolean isEmpty() {
@@ -48,7 +82,7 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        if (size != 0) {
+        if (! isEmpty()) {
             int i = 0;
             while (i < size - 1) {
                 System.out.print(items[i] + " ");
@@ -59,19 +93,37 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst() {
-        T firstitem = items[0];
-        T[] itemsNew = (T[]) new Object[size - 1];
-        System.arraycopy(itemsNew, 0, items, 1, size - 1);
-        items = itemsNew;
+        T firstitem = null;
+        if (nextFirst + 1 == items.length){
+            firstitem = items[0];
+            items[0] = null;
+            nextFirst = 0;
+        } else {
+            firstitem = items[nextFirst + 1];
+            items[nextFirst + 1] = null;
+            nextFirst += 1;
+        }
         size -= 1;
+        if (items.length >= 16 && size/items.length < 0.25) {
+            resize();
+        }
         return firstitem;
     }
 
     public T removeLast() {
-        T lastitem = items[size - 1];
+        T lastitem = null;
+        if (nextLast == 0){
+            lastitem = items[items.length - 1];
+            items[items.length - 1] = null;
+            nextLast = items.length - 1;
+        } else {
+            lastitem = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast -= 1;
+        }
         size -= 1;
-        while (items.length >= 16 && size/items.length < 0.25) {
-            resize(items.length/2);
+        if (items.length >= 16 && size/items.length < 0.25) {
+            resize();
         }
         return lastitem;
     }
