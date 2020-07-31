addbc5e08155d038fb7ec52e019e5eef4766f115
chenkoo <chenkoo@hotmail.com>
2020-07-31 16:20:32 -0400

    hw3 partly finished

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,17 +1,26 @@
 package hw3.hash;
 
+import java.lang.reflect.Array;
 import java.util.List;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
-         * Write a utility function that returns true if the given oomages
-         * have hashCodes that would distribute them fairly evenly across
-         * M buckets. To do this, convert each oomage's hashcode in the
-         * same way as in the visualizer, i.e. (& 0x7FFFFFFF) % M.
-         * and ensure that no bucket has fewer than N / 50
-         * Oomages and no bucket has more than N / 2.5 Oomages.
-         */
-        return false;
+        int N = oomages.size();
+        int min = 1;
+        int[] mArray = new int[1];
+        for (Oomage oo : oomages) {
+            int bucketNum = (oo.hashCode() & 0x7FFFFFFF) % M;
+            if (bucketNum >= mArray.length) {
+                int[] temp = new int[bucketNum + 1];
+                min += (temp.length - mArray.length);
+                System.arraycopy(mArray, 0, temp, 0, mArray.length);
+                mArray = temp;
+            }
+            mArray[bucketNum] += 1;
+            if (mArray[bucketNum] > N / 2.5) return false;
+            if (mArray[bucketNum] - 1 < N / 50.0 && mArray[bucketNum] >= N / 50.0) min -= 1;
+        }
+        if (min > 0) return false;
+        return true;
     }
 }
